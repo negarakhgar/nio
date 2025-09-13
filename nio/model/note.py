db2a717431e11e25c2b8520e2a6e60f2ad0e34d3
Negar Akhgar <negar.akhgar1375@gmail.com>
2025-09-13 13:33:52 +0200

    insert metadata , type checking

--- nio/model/note.py
+++ nio/model/note.py
@@ -2,7 +2,15 @@ from typing import Dict, List
 
 
 class Note:
-    def __init__(self, title: str, content: str, metadata: Dict[str, List[str]]):
+    def __init__(self, title: str, content: str, metadata: Dict[str, str]):
+        assert isinstance(title, str)
+        assert isinstance(content, str)
+
+        assert isinstance(metadata, dict)
+        for meta_key, meta_value in metadata.items():
+            assert isinstance(meta_key, str)
+            assert isinstance(meta_value, str)
+
         self.title = title
         self.content = content
         self.metadata = metadata
@@ -11,11 +19,19 @@ class Note:
         return f"Note: {self.title}, {self.content}"
 
     def save(self, con):
+        c = con.cursor()
+
         q = """
         INSERT INTO notes (title, content) VALUES (?, ?);
         """
-        c = con.cursor()
         c.execute(q, (self.title, self.content))
+
+        for meta_key, meta_value in self.metadata.items():
+            q = """
+            INSERT INTO metadata (title,meta_key,meta_value) VALUES (?,?,?);
+            """
+            c.execute(q, (self.title, meta_key, meta_value))
+
         con.commit()
 
     @staticmethod
